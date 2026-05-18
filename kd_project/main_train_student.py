9d2b5d3f6e56fbb315e9adefad610cfd4f40ad8c
Maria <96386495+MariaMyshko@users.noreply.github.com>
2026-05-18 17:55:09 +0300

    Update main_train_student.py

--- kd_project/main_train_student.py
+++ kd_project/main_train_student.py
@@ -4,6 +4,7 @@ from data import get_cifar10_dataloaders
 from models import StudentModel
 from train import fit
 from utils import save_checkpoint, count_parameters
+from evaluate import compute_accuracy
 
 
 def main():
@@ -32,25 +33,7 @@ def main():
             optimizer.step()
         print(f"end epoch {epoch}")
     #проверка
-    with torch.no_grad(): #отключение вычисления градиентов
-        student_model.eval() #говорим модели ничего не запоминать
-        cnt_good_predictions = 0
-        cnt_predictions = 0
-        for images, labels in test_loader:
-            predictions = student_model(images)
-            ans = [] #массив номеров самых вероятных классов для каждого батча
-            for s in predictions:
-                idx = 0
-                for i in range(len(s)):
-                    if s[i] > s[idx]:
-                        idx = i
-                ans.append(idx)
-            for i in range(len(ans)):
-                if ans[i] == labels[i]:
-                    cnt_good_predictions += 1
-            cnt_predictions += len(ans)
-        print(cnt_good_predictions / cnt_predictions) #выводим долю правильных ответов
-        student_model.train() #возвращаем модель в режим обучения
+    print(compute_accuracy(student_model, test_loader, device))
 
 if __name__ == "__main__":
     main()
