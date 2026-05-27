8f898c00cd6bf3f08568ac58783ece84a6a0df7d
Maria <96386495+MariaMyshko@users.noreply.github.com>
2026-05-27 13:35:16 +0300

    Update main_compare.py

--- kd_project/main_compare.py
+++ kd_project/main_compare.py
@@ -4,9 +4,8 @@ from data import get_cifar10_dataloaders
 from models import StudentModel, TeacherModel
 from evaluate import compute_accuracy, measure_inference_time, compute_confusion_matrix
 from utils import load_checkpoint, count_parameters
-from main_distill_student import teacher_checkpoint_path
-from main_distill_student import distill_student_checkpoint_path
-from main_train_student import student_checkpoint_path
+#from main_distill_student import teacher_checkpoint_path
+#from main_distill_student import distill_student_checkpoint_path
 
 
 
@@ -17,6 +16,7 @@ def main():
     batch_size = 128
     num_epochs = 10
     learning_rate = 0.01
+    student_checkpoint_path = "./student_model"
 
     train_loader, test_loader = get_cifar10_dataloaders(data_dir, batch_size)
 
@@ -24,9 +24,9 @@ def main():
     student_model = load_checkpoint(student_model, student_checkpoint_path, device)
     student_model.eval()
 
-    teacher_model = TeacherModel().to(device)
+    '''teacher_model = TeacherModel().to(device)
     teacher_model = load_checkpoint(teacher_model, teacher_checkpoint_path, device)
-    teacher_model.eval()
+    teacher_model.eval()'''
 
     """
     teacher_model.requires_grad_(False)
@@ -36,15 +36,15 @@ def main():
 
 
     student_acc = compute_accuracy(student_model, test_loader, device)
-    teacher_acc = compute_accuracy(teacher_model, test_loader, device)
+    #teacher_acc = compute_accuracy(teacher_model, test_loader, device)
     # distilled_acc = compute_accuracy(distilled_student_model, test_loader, device)
 
     student_params = count_parameters(student_model)
-    teacher_params = count_parameters(teacher_model)
+    #teacher_params = count_parameters(teacher_model)
     # distilled_params = count_parameters(distilled_student_model)
 
     student_time = measure_inference_time(student_model, test_loader, device)
-    teacher_time = measure_inference_time(teacher_model, test_loader, device)
+    #teacher_time = measure_inference_time(teacher_model, test_loader, device)
     # distilled_time = measure_inference_time(distilled_student_model, test_loader, device)
 
     """
@@ -52,19 +52,20 @@ def main():
                             "Accuracy": [student_acc, teacher_acc, distilled_acc],
                             "Params": [student_params, teacher_params, distilled_params],
                             "Inference Time": [student_time, teacher_time, distilled_time]}) """
-    results = pd.DataFrame({"Model": ["Baseline Student", "Teacher"],
-                            "Accuracy": [student_acc, teacher_acc],
-                            "Params": [student_params, teacher_params],
-                            "Inference Time": [student_time, teacher_time]})
+    results = pd.DataFrame({"Model": ["Baseline Student"],
+                            "Accuracy": [student_acc,],
+                            "Params": [student_params],
+                            "Inference Time": [student_time]})
     print("\n===== MODEL COMPARISON =====")
     print(
         results)
     print("\nComputing confusion matrices...")
 
 
-    compute_confusion_matrix( student_model, test_loader, device, title="Baseline Student" )
+    cm = compute_confusion_matrix( student_model, test_loader, device, title="Baseline Student" )
+    print(cm)
     # compute_confusion_matrix( distilled_student_model, test_loader, device, title="Distilled Student" )
-    compute_confusion_matrix( teacher_model, test_loader, device, title="Teacher" )
+    #compute_confusion_matrix( teacher_model, test_loader, device, title="Teacher" )
 
 
 if __name__ == "__main__":
